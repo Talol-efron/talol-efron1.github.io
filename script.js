776ce18f72c3075f3da9d1f9ae90e27db66a3a4c
Talol-efron <e205713@ie.u-ryukyu.ac.jp>
2023-01-22 22:08:22 +0900

    delete reset css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const targetElement = document.querySelectorAll(".animationTarget");
 
 document.addEventListener("scroll", function(){
     for (let i = 0; i < targetElement.length; i++) {
-        const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6
+        const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.8
         if (window.innerHeight > getElementDistance) [
             targetElement[i].classList.add("show")
         ]
