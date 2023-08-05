d52ab38b4451485c3567bfc00e5d5a380739308a
朝比奈　太郎 <e205713@ie.u-ryukyu.ac.jp>
2023-08-05 16:23:21 +0900

    chg book_list show

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const targetElement = document.querySelectorAll(".animationTarget");
-
-document.addEventListener("scroll", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    var targetElement = document.querySelectorAll('.list .entity, .animationTarget');
     for (let i = 0; i < targetElement.length; i++) {
-        const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6
-        if (window.innerHeight > getElementDistance) [
-            targetElement[i].classList.add("show")
-        ]
+        targetElement[i].style.opacity = '0';
+        targetElement[i].style.transition = 'opacity 0.2s ease-in-out 0s';
+        targetElement[i].style.visibility = 'hidden';
     }
-})
+    var counter = 0;
+    function showPictures() {
+        if (counter < targetElement.length) {
+            targetElement[counter].style.visibility = 'visible';
+            targetElement[counter].style.opacity = '1';
+            counter++;
+            setTimeout(showPictures, 200);
+        }
+    }
+    setTimeout(showPictures, 1000); // Wait for 1 second before starting to show pictures
+
+    document.addEventListener("scroll", function () {
+        for (let i = 0; i < targetElement.length; i++) {
+            const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6
+            if (window.innerHeight > getElementDistance) {
+                targetElement[i].classList.add("show")
+            }
+        }
+    });
+});
